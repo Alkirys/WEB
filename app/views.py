6a15900e84012d56f243f12f6a4aa79c63b9c15f
Alkirys <nik_manz@mail.ru>
2020-03-27 14:21:55 +0300

    add paginator

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 questions = {
    i: {'id': i, 'title': f'Question # {i}', 'tags': ['first_tag', 'second_tag']}
-   for i in range(1, 5)
+   for i in range(1, 26)
 }
 questions[5] = {'id': 5, 'title': f'Question # {5}', 'tags': ['Python', 'MySQL']}
 
@@ -14,24 +15,45 @@ answers = {
 }
 
 
+def paginate(objects_list, request):
+   try:
+      paginator = Paginator(list(objects_list), 5)
+   except ValueError:
+      paginator = list()
+
+   page_number = request.GET.get('page')
+   try:
+      page = paginator.get_page(page_number)
+
+   except PageNotAnInteger:
+      page = paginator.page(1)
+
+   except EmptyPage:
+      page = paginator.page(paginator.num_pages)
+
+   return page
+
+
 def all_questions(request):
+   pagData = paginate(questions.values(), request)
    return render(request, 'all_questions.html', {
-      'nickname': 'Alex',
-      'questions': questions.values(),
+      'questions': pagData,
    })
 
 def hot_questions(request):
+   pagData = paginate(questions.values(), request)
    return render(request, 'hot_questions.html', {
       'nickname': 'Alex',
-      'questions': questions.values(),
+      'questions': pagData,
    })
 
 def question(request, qid):
+   pagData = paginate(answers.values(), request)
    question = questions.get(qid)
    return render(request, 'question.html', {
    'nickname': 'Alex',
    'questions': question,
-   'answers': answers.values(),
+   'answers': pagData,
 })
 
 def login(request):
@@ -58,8 +80,10 @@ def tag(request, tag_name):
             render_questions[question.get('id')] = question
             continue
 
+   pagData = paginate(render_questions.values(), request)
+
    return render(request, 'tag.html', {
    'temp_tag': tag_name,
-   'questions': render_questions.values(),
+   'questions': pagData,
 })
 
