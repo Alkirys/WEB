21d562ae912861a7bb5f4e224e713f3fc829a6b8
Alkirys <nik_manz@mail.ru>
2020-04-06 12:53:22 +0300

    add

---
+++ app/models.py
@@ -0,0 +1,102 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from datetime import datetime
+from django.db.models import Sum
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255, verbose_name=u"Никнейм", default="Nickname")
+
+    def __str__(self):
+        return self.name
+
+
+class TagManager(models.Manager):
+    def popular_tags(self):
+        return self.order_by("-count")[:8]
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255, verbose_name=u"Тэг", unique=True, primary_key=True)
+    count = models.IntegerField(default=0, verbose_name=u"Число вопросов")
+
+    objects = TagManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, default=u"Unknown", verbose_name=u"Имя")
+
+    def __str__(self):
+        return self.name
+
+
+class VoteManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_rating(self):
+        return self.get_queryset().aggregate(Sum('vote')).get('vote__sum') or 0
+
+    def likes(self):
+        return self.get_queryset().filter(vote__gt=0)
+
+    def dislikes(self):
+        return self.get_queryset().filter(vote__lt=0)
+
+
+class Vote(models.Model):
+    votes = ((1, 'like'), (-1, 'dislike'))
+    vote = models.IntegerField(choices=votes, verbose_name=u"Голосование")
+    name = models.CharField(max_length=255, verbose_name=u"Пользователь")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.IntegerField()
+    content_obj = GenericForeignKey()
+
+    objects = VoteManager()
+
+
+class QuestionManager(models.Manager):
+    def by_tag(self, tag):
+        return self.filter(tags__title=tag)
+
+    def most_popular(self):
+        return self.order_by('-like')
+
+
+class Question(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name=u"Заголовок вопроса")
+    content = models.TextField(verbose_name=u"Текст вопроса")
+    date = models.DateTimeField(default=datetime.now, verbose_name=u"Время создания вопроса")
+    is_active = models.BooleanField(default=True, verbose_name=u"Опубликован ли вопрос")
+    tags = models.ManyToManyField(Tag, blank=True)
+    like = GenericRelation(Vote, related_query_name=u'Question')
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date']
+        verbose_name = u"Вопрос"
+        verbose_name_plural = u"Вопросы"
+
+
+class Answer(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField(verbose_name=u"Текст ответа")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=datetime.now, verbose_name=u"Время ответа")
+    like = GenericRelation(Vote, related_query_name=u'Answer')
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        verbose_name = u"Ответ"
+        verbose_name_plural = u"Ответы"
